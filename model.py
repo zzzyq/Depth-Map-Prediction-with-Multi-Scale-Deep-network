71130a57dde645662ef14164e85f49feca31c340
CiferU4 <zx4@uw.edu>
2018-03-14 14:37:47 -0700

    Add files via upload

--- model.py
+++ model.py
@@ -7,12 +7,10 @@ import torch.nn.functional as F
 class coarseNet(nn.Module):
     def __init__(self,init_weights=True):
         super(coarseNet, self).__init__()
-        self.conv1 = nn.Conv2d(3,96,kernel_size=11,stride=4,padding=0)
-        self.conv2 = nn.Conv2d(96,256,kernel_size=5,stride=1,padding=0)
-        self.conv3 = nn.Conv2d(256,384,kernel_size=1,stride=1,padding=0)
-        self.conv4 = nn.Conv2d(384,384,kernel_size=1,stride=1,padding=0)
-        self.conv5 = nn.Conv2d(384,256,kernel_size=1,stride=1,padding=0)
-        self.fc1 = nn.Linear(10 * 6 * 256, 4096)
+        self.conv1 = nn.Conv2d(3,128,kernel_size=3,stride=4,padding=0)
+        self.conv2 = nn.Conv2d(128,128,kernel_size=3,stride=1,padding=0)
+        self.conv3 = nn.Conv2d(128,128,kernel_size=3,stride=1,padding=0)
+        self.fc1 = nn.Linear(94208, 4096)
         self.fc2 = nn.Linear(4096, 4070)
         if init_weights:
             self._initialize_weights()
@@ -22,8 +20,7 @@ class coarseNet(nn.Module):
         x = F.max_pool2d(self.conv1(x), 3, stride=2)
         x = F.max_pool2d(self.conv2(x), 2, stride =1)
         x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
+        x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = x.view(-1, 55, 74)
